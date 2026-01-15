c668af0105d6ea51ff8a03543be1fb22d83e0f9c
harvspex <88419020+harvspex@users.noreply.github.com>
2026-01-16 10:54:21 +1100

    pre blacklist overhaul commit

--- src/classes/blacklist.py
+++ src/classes/blacklist.py
@@ -1,44 +1,66 @@
-from classes.data import _BaseData, Config
+from classes.data import _BaseData, Config, Games
 import hashlib
 from pathlib import Path
 from typing import override
 
-ALL_SYSTEMS = 'All Systems'
-
 class Blacklist(_BaseData):
+    ALL_SYSTEMS = 'All Systems'
+
     def __init__(self, config: Config):
         self.config = config
         filepath = Path.cwd() / 'blacklist.txt'
         super().__init__(filepath)
         self.save()
 
+    def apply(self, games: Games, will_check_hash: bool=True):
+        bl_hash = self.get_hash()
+
+        if will_check_hash and bl_hash == self.config.data['bl_hash']:
+            return
+
+        for system in games.data:
+            bl_id = self.config.data['systems'][system]['id']
+            for game in system:
+                for phrase in self.data[self.ALL_SYSTEMS]:
+                    if phrase in game['name']:
+                        game['bl'] = True
+                for phrase in self.data['bl_id']:
+                    if phrase in game['name']:
+                        game['bl'] = True
+            # TODO This sucks
+
     def get_hash(self):
         with open(self.filepath, 'rb') as f:
-            hash = hashlib.md5(f.read()).hexdigest()
-            self.config.data['bl_hash'] = hash
-            return hash
+            return hashlib.md5(f.read()).hexdigest()
 
     @override
     def load(self) -> dict:
-        blacklist = {ALL_SYSTEMS: {'id': ALL_SYSTEMS}}
+        # Init
+        blacklist = {self.ALL_SYSTEMS: {'id': self.ALL_SYSTEMS}}
         for id, sys in self.config.data['systems'].items():
             blacklist[sys['bl']] = {'id': id}
+
+        # Read
         try:
             with open(self.filepath, 'r') as f:
                 bl_file = f.read()
         except FileNotFoundError:
             return blacklist
 
+        # Parse
         bl_file = bl_file.split('#')
         bl_file = bl_file[1:] if bl_file[0] != '' else bl_file
         bl_file = [_.splitlines() for _ in bl_file if _]
         bl_file = [[_.strip() for _ in sys if _] for sys in bl_file]
         bl_file = {_[0]:_[1:] for _ in bl_file}
+
+        # Combine
         for key in blacklist:
             try:
                 blacklist[key]['list'] = bl_file[key]
             except KeyError:
                 blacklist[key]['list'] = []
+
         return blacklist
 
     @override
--- src/classes/vimmit.py
+++ src/classes/vimmit.py
@@ -16,15 +16,21 @@ class Vimmit:
     def _handle_blacklist(
         self,
         config: Config,
-        systems: dict,
         will_check_hash: bool=True
     ):
         blacklist = Blacklist(config)
+        bl_hash = blacklist.get_hash()
+
         if will_check_hash:
             old_hash = self.config.data['bl_hash']
-            if old_hash == blacklist.get_hash():
+            if old_hash == bl_hash:
                 return
-    
+
+        for system in blacklist.data.values():
+            sys_id = system['id']
+            bl = system['list']
+            
+
         # for sys_id, bl_id in self.config.get_blacklist_keys().items():
         #     if sys_id in systems:
         #         # TODO: Flag blacklisted items
@@ -56,19 +62,24 @@ class Vimmit:
             # TODO: Make nicer?
             self.config.data['systems'] = scrape_systems(self.config.data['base_url'])
 
-        systems = {v['id']: v['name'] for k, v in self.config.data['systems'].items() if k in self.args.systems}
+        # TODO: Make this a set of IDs?
+        selected_systems = {v['id']: v['name'] for k, v in self.config.data['systems'].items() if k in self.args.systems}
+
         if not self.args.download:
-            self._handle_blacklist(self.config, systems, will_check_hash=True)
-            vimm_roller = VimmRoller(self.games, self.config, systems)
+            self._handle_blacklist(self.config, will_check_hash=True)
+            vimm_roller = VimmRoller(self.games, self.config, selected_systems)
             vimm_roller.roll()
             return
 
         from requests import ConnectionError
         try:
-            self._handle_scrape(systems)
-            self._handle_blacklist(self.config, systems, will_check_hash=False)
+            self._handle_scrape(selected_systems)
+            self._handle_blacklist(self.config, will_check_hash=False)
         except (AttributeError, ConnectionError):
             print('That didn\'t work. Resetting base url.') # TODO: Better handling and message
             self.config['base_url'] = None
             self.config.save()
             return
+
+        self.games.save()
+        self.config.save()
