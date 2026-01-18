22fd7918889787868731983c5b1827dbac958b57
harvspex <88419020+harvspex@users.noreply.github.com>
2026-01-18 14:00:32 +1100

    added console singleton

--- vimmit/run.py
+++ vimmit/run.py
@@ -1,11 +1,10 @@
 from classes.vimmit import Vimmit
 from classes.exceptions import *
-from rich.console import Console
+from utils.cli import console
 
 # TODO: setup or way to install
 
 def main():
-    console = Console()
     vimmit = Vimmit()
     try:
         vimmit.run()
@@ -14,9 +13,9 @@ def main():
     except ScrapeError:
         console.print('[red]Download error - Resetting config.[/red]')
         vimmit.reset_config(reset_all=True)
-    except ConnectionError:
-        console.print('[red]Connection error - Resetting base url.[/red]')
-        vimmit.reset_config('base_url')
+    # except ConnectionError:
+    #     console.print('[red]Connection error - Resetting base url.[/red]')
+    #     vimmit.reset_config('base_url')
     except KeyboardInterrupt:
         pass
 
