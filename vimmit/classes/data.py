2b6be6e9c579f131876732faf299c9bf08ea7685
harvspex <88419020+harvspex@users.noreply.github.com>
2026-01-18 14:19:40 +1100

    added dump_history

--- vimmit/classes/data.py
+++ vimmit/classes/data.py
@@ -38,6 +38,16 @@ class Games(_BaseData):
         with open(filepath, 'w') as f:
             json.dump(self.data, f, indent=2)
 
+    def dump_history(self, config: Config, filepath: Path):
+        extract_names = lambda data: [f'{game['name']}\n' for game in data.values() if game.get('seen', False)]
+        history = {system: extract_names(games) for system, games in self.data.items()}
+        with open(filepath, 'w') as f:
+            for sys_id, games in history.items():
+                bl_id = config.data['systems'][sys_id]['bl_id']
+                f.write(f'# {bl_id}\n')
+                f.writelines(games)
+                f.write('\n')
+
 
 class Config(_BaseData):
     def __init__(self):
