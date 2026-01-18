9253e90f4dd9143dc18bf7cb702e29221736a273
harvspex <88419020+harvspex@users.noreply.github.com>
2026-01-18 15:04:09 +1100

    import export wip

--- vimmit/classes/data.py
+++ vimmit/classes/data.py
@@ -102,3 +102,39 @@ class Blacklist(_BaseData):
                 f.write(f'# {bl_id}\n')
                 f.writelines([f'{v}\n' for v in values])
                 f.write('\n')
+
+
+class ImportExport(_BaseData):
+    # TODO: Read Exceptions?
+    # TODO: Import blacklist?
+
+    def __init__(self, filepath):
+        self.filepath = self._validate_filepath(filepath)
+
+    @staticmethod
+    def _validate_filepath(filepath: str):
+        # TODO
+        ...
+        return filepath
+
+    @staticmethod
+    def _update(old_data: dict, new_data: dict) -> dict:
+        # NOTE: Updated gamelist may lose alphabetic ordering
+        for k, v in new_data.items():
+            if k not in old_data:
+                old_data[k] = v
+            elif isinstance(old_data[k], dict) and isinstance(v, dict):
+                ImportExport._update(old_data[k], v)
+        return old_data
+
+    def import_file(self, old_config: Config, old_games: Games) -> tuple[Config, Games]:
+        # Updates without overwriting
+        data = self.load()
+        return self._update(old_config, data['config']), self._update(old_games, data['games'])
+
+    def export_file(self, config: Config, games: Games):
+        self.data = {
+            'config': config,
+            'games': games
+        }
+        self.save()
