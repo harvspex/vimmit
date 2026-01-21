aab642af6cc07fd5c6c326128e7c3d7264f83114
harvspex <88419020+harvspex@users.noreply.github.com>
2026-01-21 11:44:50 +1100

    disabled save on Config init

--- vimmit/data/config.py
+++ vimmit/data/config.py
@@ -4,4 +4,4 @@ from data.base_data import BaseData, DATA_DIR
 class Config(BaseData):
     def __init__(self):
         super().__init__(DATA_DIR, 'config.dat')
-        self.save()
+        # self.save() # TODO: Test if OK to disable
