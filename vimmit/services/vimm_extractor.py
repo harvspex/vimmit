dbe64d536519b7eb8be9e31d7889e2e6d2bda601
harvspex <88419020+harvspex@users.noreply.github.com>
2026-01-23 09:25:22 +1100

    planning

---
+++ vimmit/services/vimm_extractor.py
@@ -0,0 +1,10 @@
+# TODO:
+# - Setup downloads folder and destination folder if needed
+# - Match string similarity of seen games to games in downloads folder
+# - Match extension == .zip or .7z
+# - Extract games to dir "Game Name (region)"
+# - Flag games as extracted
+# - Delete archive if will_delete == True
+#
+# Need to change importer to only import game IDs, names, and seen data
+# May need to save region data during scraping
